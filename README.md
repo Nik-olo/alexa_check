c114a924ee61c98f3e9684a2cfaa6e1a7c1b4c14
rootVIII <colleyloyejames@gmail.com>
2018-10-14 02:45:30 -0400

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+## Check a domain/website's Amazon Alexa Ranking via the terminal/cmd line
+<br><br>
+<img src="https://github.com/rootVIII/alexa_check/blob/master/output_screenshot.png" alt="example1" height="675" width="950"><hr>
+<br><br>
+pass a domain as argument with -d option
+<br><br>
+usage:
+<br><br>
+python alexa_check.py -d example_domain.com
+<br><br>
+This was developed on Ubuntu 16.04.4 LTS.
+<hr>
+<b>Author: James Loye Colley  14OCT2018</b><br><br>
---
+++ alexa_check.py
@@ -0,0 +1,49 @@
+#! /usr/bin/python3
+# check a domain's Alexa rank via command line
+# pass domain as argument with -d option
+# usage:
+# python alexa_check.py -d example_domain.com
+import requests
+import argparse
+import re
+from sys import exit
+
+
+class AlexaCheck:
+    def __init__(self, domain):
+        self.url = "https://www.alexa.com/siteinfo/"
+        self.url += domain
+        self.ranks = []
+
+    def __search_regex(self, regex, phrase):
+        match = re.search(regex, phrase)
+        if match:
+            return match.group(1)
+
+    def check(self):
+        r = requests.get(self.url)
+        page = str(r.text).split("\n")
+        found_global = False
+        for line in page:
+            if "</strong>" in line and any(char.isdigit() for char in line):
+                perspective = line.replace(',', '')
+                found = self.__search_regex("(\d+)\s+<", perspective)
+                if found and found[:1] != "0":
+                    if not found_global:
+                        self.ranks.append(("global_rank", found))
+                        found_global = True
+            if "Flag" in line and "nbsp" in line:
+                perspective = line.replace(',', '')
+                country = self.__search_regex(".*\w+;(.*)</a>", perspective)
+                country_rank = self.__search_regex(".*>(\d+)<", perspective)
+                self.ranks.append((country, country_rank))
+        for alexa_rank_tuple in self.ranks:
+            yield alexa_rank_tuple
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Enter -d domain.com as argument')
+    parser.add_argument('-d', '--domain', metavar='', required=True, help='Enter a domain: example.com')
+    domain = vars(parser.parse_args())
+    for rank_tuple in AlexaCheck(domain['domain']).check():
+        print(rank_tuple)
